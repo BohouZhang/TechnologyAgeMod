e3deaec211a9fca654517fb9d2114b5c6137b24a
BohouZhang <bohou0519@gamil.com>
2022-08-19 23:41:14 +0800

    [feat]初始化一些基本设置，包括modId、作者姓名、简介的修改，以及对Mod的Java文件的初始化。使得整个工程不再是examplemod，而是做好了全部的开发准备。同时确保工程能正常运行

---
+++ src/main/java/com/bohou/technology_age/TechnologyAge.java
@@ -0,0 +1,37 @@
+package com.bohou.technology_age;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(TechnologyAge.MOD_ID)
+public class TechnologyAge {
+    public static final String MOD_ID = "technology_age";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TechnologyAge() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("[technology-age] client mod is going to start ...");
+        }
+    }
+}
