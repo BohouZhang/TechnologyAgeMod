81f545a5c9ac4b2cc9a7356f03c606c51e4d19aa
BohouZhang <bohou0519@gamil.com>
2022-08-25 00:21:26 +0800

    [feat]增加所有颜色的防爆混凝土，然后删掉了以前测试用的混凝土方块

--- src/main/java/com/bohou/technology_age/block/ModBlocks.java
+++ src/main/java/com/bohou/technology_age/block/ModBlocks.java
@@ -3,6 +3,7 @@ package com.bohou.technology_age.block;
 import java.util.function.Supplier;
 
 import com.bohou.technology_age.TechnologyAge;
+import com.bohou.technology_age.enums.ExplosionProofConcreteEnum;
 import com.bohou.technology_age.item.ModItems;
 import com.bohou.technology_age.tab.ModCreativeModeTab;
 
@@ -26,18 +27,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
         DeferredRegister.create(ForgeRegistries.BLOCKS, TechnologyAge.MOD_ID);
 
-    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE = registerBlock(
-        "explosion_proof_concrete",
-        ModCreativeModeTab.BUILDING_BLOCK_TAB,
-        () -> new Block(
-            BlockBehaviour.Properties.of(Material.STONE)
-                .strength(0.5f)
-                .explosionResistance(18000000.0f)
-                .requiresCorrectToolForDrops()
-        )
-    );
-
-    private static <T extends Block> RegistryObject<T> registerBlock(String name,CreativeModeTab tab, Supplier<T> block ) {
+    protected static <T extends Block> RegistryObject<T> registerBlock(String name,CreativeModeTab tab, Supplier<T> block ) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
             registerBlockItem(name, toReturn, tab);
         return toReturn;
@@ -47,9 +37,165 @@ public class ModBlocks {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
 
-
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
-    
+
+    // -------------------- 防爆混凝土 开始 -------------------- //
+
+    /**
+     * 防爆混凝土通用配置
+     */
+    private static BlockBehaviour.Properties explosionProofConcreteProperty = BlockBehaviour.Properties
+        .of(Material.STONE)
+        .strength(0.5f)
+        .explosionResistance(18000000.0f)
+        .requiresCorrectToolForDrops();
+
+    /**
+     * 白色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_WHITE = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_WHITE.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 橙色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_ORANGE = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_ORANGE.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 品红色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_MAGENTA = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_MAGENTA.getName(), 
+        ModCreativeModeTab.BUILDING_BLOCK_TAB, 
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 淡蓝色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_LIGHT_BLUE = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_LIGHT_BLUE.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 黄色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_YELLOW = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_YELLOW.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 黄绿色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_LIME = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_LIME.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 粉红色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_PINK = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_PINK.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 灰色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_GRAY = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_GRAY.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 淡灰色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_LIGHT_GRAY = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_LIGHT_GRAY.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 青色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_CYAN = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_CYAN.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 紫色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_PURPLE = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_PURPLE.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 蓝色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_BLUE = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_BLUE.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 棕色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_BROWN = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_BROWN.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 绿色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_GREEN = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_GREEN.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 红色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_RED = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_RED.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    /**
+     * 黑色防爆混凝土
+     */
+    public static final RegistryObject<Block> EXPLOSION_PROOF_CONCRETE_BLACK = ModBlocks.registerBlock(
+        ExplosionProofConcreteEnum.EXPLOSION_PROOF_CONCRETE_BLACK.getName(),
+        ModCreativeModeTab.BUILDING_BLOCK_TAB,
+        () -> new Block(explosionProofConcreteProperty)
+    );
+
+    // -------------------- 防爆混凝土 结束 -------------------- //
+
 }
---
+++ src/main/java/com/bohou/technology_age/enums/ExplosionProofConcreteEnum.java
@@ -0,0 +1,81 @@
+package com.bohou.technology_age.enums;
+
+public enum ExplosionProofConcreteEnum {
+    
+    /**
+     * 白色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_WHITE("explosion_proof_concrete_white"),
+    /**
+     * 橙色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_ORANGE("explosion_proof_concrete_orange"),
+    /**
+     * 品红色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_MAGENTA("explosion_proof_concrete_magenta"),
+    /**
+     * 淡蓝色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_LIGHT_BLUE("explosion_proof_concrete_light_blue"),
+    /**
+     * 黄色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_YELLOW("explosion_proof_concrete_yellow"),
+    /**
+     * 黄绿色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_LIME("explosion_proof_concrete_lime"),
+    /**
+     * 粉红色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_PINK("explosion_proof_concrete_pink"),
+    /**
+     * 灰色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_GRAY("explosion_proof_concrete_gray"),
+    /**
+     * 淡灰色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_LIGHT_GRAY("explosion_proof_concrete_light_gray"),
+    /**
+     * 青色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_CYAN("explosion_proof_concrete_cyan"),
+    /**
+     * 紫色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_PURPLE("explosion_proof_concrete_purple"),
+    /**
+     * 蓝色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_BLUE("explosion_proof_concrete_blue"),
+    /**
+     * 棕色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_BROWN("explosion_proof_concrete_brown"),
+    /**
+     * 绿色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_GREEN("explosion_proof_concrete_green"),
+    /**
+     * 红色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_RED("explosion_proof_concrete_red"),
+    /**
+     * 黑色防爆混凝土
+     */
+    EXPLOSION_PROOF_CONCRETE_BLACK("explosion_proof_concrete_black"),
+    ;
+
+    private String name;
+
+    ExplosionProofConcreteEnum(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
