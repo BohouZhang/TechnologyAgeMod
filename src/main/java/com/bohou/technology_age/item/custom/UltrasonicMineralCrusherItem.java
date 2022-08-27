206e685a6468ab26b8893f529a51ef06cd5283a4
BohouZhang <bohou0519@gamil.com>
2022-08-27 13:22:37 +0800

    [feat]完成「超声波碎矿仪」代码逻辑，破坏方块时一并摧毁周围5格范围内的相同方块

--- src/main/java/com/bohou/technology_age/item/custom/UltrasonicMineralCrusherItem.java
+++ src/main/java/com/bohou/technology_age/item/custom/UltrasonicMineralCrusherItem.java
@@ -1,42 +1,64 @@
 package com.bohou.technology_age.item.custom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class UltrasonicMineralCrusherItem extends Item {
+
+    private final int DESTROY_DISTANCE = 5;
 
     public UltrasonicMineralCrusherItem(Properties properties) {
         super(properties);
     }
 
     /**
-     * 右键使用，如果已经存了方块的信息，摧毁一定区域内所有相同方块
+     * 使用超声波碎矿仪摧毁方块时，一并摧毁该方块周围5格内的相同方块
      */
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        if (level.isClientSide) {
-            if (hand == InteractionHand.MAIN_HAND) {
-                // 获取玩家当前点击的方块的信息
-                BlockState currentBlock = player.getBlockStateOn();
-                player.sendSystemMessage(Component.literal("主手点击" + currentBlock.getBlock().getName()));
-            }
+    public boolean onBlockStartBreak(ItemStack itemstack, BlockPos pos, Player player) {
+        if (!player.level.isClientSide()) {
+            List<BlockPos> blockToBeDestroy = getAroundBlockPos(pos, player);
+            blockToBeDestroy.forEach(blockPos -> {
+                player.level.destroyBlock(blockPos, true);
+            });
         }
-        return super.use(level, player, hand);
+        return super.onBlockStartBreak(itemstack, pos, player);
     }
 
     /**
-     * 禁止使用这个物品直接破坏方块
+     * 根据一个方块的位置，获取它周围5格子内相同方块的坐标
+     * @param blockPos 方块的位置信息
+     * @param player 玩家信息
+     * @return
      */
-    @Override
-    public boolean canAttackBlock(BlockState blockState, Level level, BlockPos blockPos, Player player) {
-        return false;
+    private List<BlockPos> getAroundBlockPos(BlockPos blockPos, Player player) {
+        List<BlockPos> blockPosList = new ArrayList<>();
+        // 获取当前方块的信息
+        BlockState currentBlock = player.level.getBlockState(blockPos);
+        String currentBlockType = currentBlock.toString();
+        // 获取当前方块的坐标
+        int x = blockPos.getX();
+        int y = blockPos.getY();
+        int z = blockPos.getZ();
+        // 遍历范围内所有方块，如果类型跟该方块一样，就加到blockPosList中
+        for (int i = x - DESTROY_DISTANCE; i <= x + DESTROY_DISTANCE; i++) {
+            for (int j = y - DESTROY_DISTANCE; j <= y + DESTROY_DISTANCE; j++) {
+                for (int k = z - DESTROY_DISTANCE; k <= z + DESTROY_DISTANCE; k++) {
+                    BlockPos tempBLockPos = new BlockPos(i, j, k);
+                    BlockState tempBlockState = player.level.getBlockState(tempBLockPos);
+                    if (currentBlockType.equals(tempBlockState.toString())) {
+                        blockPosList.add(tempBLockPos);
+                    }
+                }
+            }
+        }
+        return blockPosList;
     }
 
 }
